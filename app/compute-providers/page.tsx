24a0dadad2802f0eed6b3e5adfd1fa8465d92fee
Ryan Leung <ryancleung@gmail.com>
2024-03-08 18:27:33 -0500

    Add scaffolding for pages

---
+++ app/compute-providers/page.tsx
@@ -0,0 +1,9 @@
+import { Text } from '@tremor/react';
+
+export default function ComputeProvidersPage() {
+  return (
+    <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <Text>Todo</Text>
+    </main>
+  );
+}
