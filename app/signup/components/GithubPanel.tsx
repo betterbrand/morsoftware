d24865919d7d8c3ed31e1ddd6b6f5a7d0c64baf9
Brian Wang <brian.wang19@gmail.com>
2024-03-12 22:08:02 -0400

    add breadcrumbs and local memory for signup page (#23)
    * add breadcrumbs and local memory for signup page
    
    * nit

--- app/signup/components/GithubPanel.tsx
+++ app/signup/components/GithubPanel.tsx
@@ -1,19 +1,25 @@
-import SectionHeader from './SectionHeader';
 import { PANELS } from '../constants';
+import GithubCatLogoWhite from '../../public/images/github-cat-logo-white.svg';
+import Image from 'next/image';
 
 type EmailPanelProps = { setSelectedPanel: (panel: string) => void };
 
 const GithubPanel = ({ setSelectedPanel }: EmailPanelProps) => {
   return (
-    <div className="flex flex-col gap-8">
-      <SectionHeader header="Connect your Github account" subheader="" />
-
-      <div className="mb-4">
+    <div className="flex flex-col  w-full h-full">
+      <div className="text-xl font-semibold">Connect your Github account</div>
+      <div className="grow flex justify-center items-center">
         <button
           onClick={() => setSelectedPanel(PANELS.MAIN)}
           type="submit"
-          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="inline-flex items-center justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
+          <Image
+            src={GithubCatLogoWhite}
+            alt="Github logo"
+            height={32}
+            style={{ marginRight: 8 }}
+          />
           Connect your Github account
         </button>
       </div>
@@ -22,17 +28,10 @@ const GithubPanel = ({ setSelectedPanel }: EmailPanelProps) => {
         <button
           onClick={() => setSelectedPanel(PANELS.MAIN)}
           type="button"
-          className="mr-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="mr-2 mb-2 inline-flex justify-center py-2 px-4 border shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           Go Back
         </button>
-        {/* <button
-          onClick={() => setSelectedPanel(PANELS.MAIN)}
-          type="submit"
-          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Continue
-        </button> */}
       </div>
     </div>
   );
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,30 +1,61 @@
 'use client';
 
 import { useState } from 'react';
-
-import { classNames } from '../helpers';
 import { Card } from '@tremor/react';
 import { PANELS } from './constants';
-import SectionHeader from './components/SectionHeader';
-import SubsectionHeader from './components/SubsectionHeader';
+
 import SignupForm from './components/SignupForm';
 import GithubPanel from './components/GithubPanel';
+import { FormValues } from './types';
+
+type BreadcrumbProps = {
+  selectedPanel: string;
+};
+
+const Breadcrumb = ({ selectedPanel }: BreadcrumbProps) => {
+  return (
+    <div className="flex items-center justify-center text-gray-500 text-sm">
+      <div>1. Create Account</div>
+      {selectedPanel === PANELS.GITHUB && (
+        <>
+          <svg
+            className="fill-current w-3 h-3 mx-3 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M7.03 14.91a.75.75 0 001.06 0l5-5a.75.75 0 000-1.06l-5-5a.75.75 0 10-1.06 1.06L11.44 9l-4.41 4.44a.75.75 0 000 1.06z" />
+          </svg>
+          <div className="text-sm">2. Attach Github</div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default function SettingsPage() {
   const [selectedPanel, setSelectedPanel] = useState(PANELS.MAIN);
+  const [signupFormFields, setSignupFormFields] = useState<FormValues>({
+    name: '',
+    email: '',
+    walletAddress: '',
+    tosAccepted: false
+  });
 
   return (
     <main className="p-4 md:p-10 flex justify-center">
-      {/* <div className="max-w-[880px] max-h-[660px] mx-auto"> */}
-      <Card className="py-3 px-8 max-w-[880px] h-screen max-h-[600px] overflow-y-scroll">
+      <Card className="py-3 px-8 max-w-[880px] h-screen max-h-[600px] overflow-y-scroll flex flex-col">
         {selectedPanel === PANELS.MAIN && (
-          <SignupForm setSelectedPanel={setSelectedPanel} />
+          <SignupForm
+            setSelectedPanel={setSelectedPanel}
+            signupFormFields={signupFormFields}
+            setSignupFormFields={setSignupFormFields}
+          />
         )}
         {selectedPanel === PANELS.GITHUB && (
           <GithubPanel setSelectedPanel={setSelectedPanel} />
         )}
+        <Breadcrumb selectedPanel={selectedPanel} />
       </Card>
-      {/* </div> */}
     </main>
   );
 }
