7af273979beceef529d6ee727bb15aaecd19cc1b
Ryan Leung <ryancleung@gmail.com>
2024-03-13 18:22:11 -0400

    more

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -11,31 +11,22 @@ const prisma = new PrismaClient();
  * @returns 
  */
 export async function POST(request: Request) {
-  console.log('POST /api/register');
   const session = await auth();
   const data = await request.json();
-  console.log('data:', data);
 
   if (!session?.user) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
 
   try {
-    const user = await prisma.user.upsert({
-      where: { email: data.email as string },
-      update: { 
-        username: data.username as string, 
-        walletAddress: data.walletAddress as string,
-        githubId: data.githubId as string
-      },
-      create: { 
-        username: data.username as string, 
-        email: data.email as string, 
-        walletAddress: data.walletAddress as string, 
-        developerId: 'test-id', 
-        githubId: data.githubId as string
-      },
-    });
+    const user = await prisma.user.create({
+      data: {
+        developerId: 'testdeveloperid', // TODO: Generate this with a lib function
+        username: data.username,
+        email: data.email,
+        walletAddress: data.walletAddress,
+        githubId: data.githubId,
+      }});
 
     return NextResponse.json(user);
   } catch (error) {
