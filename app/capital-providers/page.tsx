8439b6e2adc3609726e576e47c0a805146ae98b3
Ryan Leung <ryancleung@gmail.com>
2024-03-16 14:30:42 -0400

    Remove unused tabs (#25)

--- app/capital-providers/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from '@tremor/react';
-
-export default function CapitalProvidersPage() {
-  return (
-    <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Text>Todo</Text>
-    </main>
-  );
-}
--- app/compute-providers/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from '@tremor/react';
-
-export default function ComputeProvidersPage() {
-  return (
-    <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Text>Todo</Text>
-    </main>
-  );
-}
