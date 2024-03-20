c89a7df740e35b58914b5af00f8efd4f2196f5c8
Ryan Leung <ryancleung@gmail.com>
2024-03-19 23:10:07 -0400

    Reenable middleware

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,3 @@
 export { GET, POST } from '../../../auth';
-export const runtime = 'nodejs';
+export const runtime = 'edge';
 // Note: Change runtime = "nodejs" if things aren't working https://vercel.com/docs/functions/runtimes https://nextjs.org/docs/app/building-your-application/rendering/edge-and-nodejs-runtimes 
