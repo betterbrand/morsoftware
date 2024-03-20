63dfd0159998148d9fd4063ab0ae0cd5a14f5628
lusol <brian.wang19@gmail.com>
2024-03-20 01:49:22 -0400

    home page change

--- app/components/githubLoginButton.tsx
+++ app/components/githubLoginButton.tsx
@@ -15,7 +15,7 @@ const GithubLoginButton = () => {
     <button
       onClick={handleSubmit}
       type="submit"
-      className="inline-flex items-center justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-emerald-900 hover:bg-emerald-950 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      className="inline-flex items-center justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
     >
       <Image
         src={GithubCatLogoWhite}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Image from 'next/image';
-import MorLogo from './public/images/mor-logo.svg';
-
+import BackgroundVideo from './public/videos/black_background.mp4';
 import GithubLoginButton from './components/githubLoginButton';
 import { auth } from './auth';
 import { redirect } from 'next/navigation';
@@ -24,13 +22,23 @@ export default async function IndexPage({
   }
 
   return (
-    <main className="p-4 md:p-10 mx-auto max-w-7xl flex flex-col items-center">
-      <Image src={MorLogo} alt="MOR logo" height={200} className="mb-8" />
-      <div className="text-3xl font-bold mb-14">MOR Software</div>
-      <div className="text-base">
+    <main className="p-4 md:p-10 mx-auto max-w-7xl h-[100vh] flex flex-col items-center justify-center">
+      <video
+        autoPlay
+        muted
+        loop
+        className="absolute z-[-1] w-full h-full object-cover"
+      >
+        <source src={BackgroundVideo} type="video/mp4" />
+      </video>
+      {/* <Image src={MorLogo} alt="MOR logo" height={200} className="mb-8" /> */}
+      <div className="text-5xl font-bold mb-14 uppercase text-white">
+        MOR Software
+      </div>
+      <div className="text-base text-gray-400">
         Become a contributor to the future of open source AI development
       </div>
-      <div className="text-base mb-12">
+      <div className="text-base mb-12 text-gray-400 text-center">
         Sign into github to register as a DEVELOPER for the Morpheus AI project
       </div>
       <GithubLoginButton />
