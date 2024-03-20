ae844b92507ef80ae315ffd98144a1d38bfb9f0d
Brian Wang <brian.wang19@gmail.com>
2024-03-20 12:50:14 -0400

    css refactor (#40)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import BackgroundVideo from './public/videos/black_background.mp4';
 import GithubLoginButton from './components/githubLoginButton';
+import BackgroundImage from './public/images/black-background.jpg';
 import { auth } from './auth';
 import { redirect } from 'next/navigation';
+
 interface User {
   id: number;
   name: string;
@@ -22,20 +24,14 @@ export default async function IndexPage({
   }
 
   return (
-    <main className="p-4 md:p-10 mx-auto max-w-7xl h-[100vh] flex flex-col items-center justify-center">
-      <video
-        autoPlay
-        muted
-        loop
-        className="absolute z-[-1] w-full h-full object-cover"
-      >
-        <source src={BackgroundVideo} type="video/mp4" />
-      </video>
-      {/* <Image src={MorLogo} alt="MOR logo" height={200} className="mb-8" /> */}
-      <div className="text-5xl font-bold mb-14 uppercase text-white">
+    <main
+      className="p-4 md:p-10 w-[100vw] h-[100vh] flex flex-col items-center justify-center bg-no-repeat bg-cover bg-center bg-fixed"
+      style={{ backgroundImage: `url(${BackgroundImage.src})` }}
+    >
+      <div className="text-5xl font-bold mb-14 uppercase text-white text-center">
         MOR Software
       </div>
-      <div className="text-base text-gray-400">
+      <div className="text-base text-gray-400 text-center">
         Become a contributor to the future of open source AI development
       </div>
       <div className="text-base mb-12 text-gray-400 text-center">
