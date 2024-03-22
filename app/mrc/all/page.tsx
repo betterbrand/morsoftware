fc763f83b320f676590c53f8b9acc8740c156c1f
Brian Wang <brian.wang19@gmail.com>
2024-03-22 16:55:34 -0400

    mrc all page scaffold (#57)

---
+++ app/mrc/all/page.tsx
@@ -0,0 +1,9 @@
+import MrcAllPageClient from './MrcAllPageClient';
+
+export default async function MrcAllPage() {
+  return (
+    <main className="p-4 md:p-10 flex justify-center">
+      <MrcAllPageClient />
+    </main>
+  );
+}
